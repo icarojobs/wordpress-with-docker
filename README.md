b1b6440d63973534d898ed6bfd5086e8f0a8ffc4
Tio Jobs <icarojobsoficial@gmail.com>
2023-09-14 09:24:24 -0300

    chore: created base to work with wordpress using docker-compose

---
+++ README.md
@@ -0,0 +1,159 @@
+# WPDC - WordPress Docker Compose
+
+Easy WordPress development with Docker and Docker Compose.
+
+With this project you can quickly run the following:
+
+- [WordPress and WP CLI](https://hub.docker.com/_/wordpress/)
+- [phpMyAdmin](https://hub.docker.com/r/phpmyadmin/phpmyadmin/)
+- [MySQL](https://hub.docker.com/_/mysql/)
+
+Contents:
+
+- [Requirements](#requirements)
+- [Configuration](#configuration)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## Requirements
+
+Make sure you have the latest versions of **Docker** and **Docker Compose** installed on your machine.
+
+Clone this repository or copy the files from this repository into a new folder. In the **docker-compose.yml** file you may change the IP address (in case you run multiple containers) or the database from MySQL to MariaDB.
+
+Make sure to [add your user to the `docker` group](https://docs.docker.com/install/linux/linux-postinstall/#manage-docker-as-a-non-root-user) when using Linux.
+
+## Configuration
+
+Copy the example environment into `.env`
+
+```
+cp env.example .env
+```
+
+Edit the `.env` file to change the default IP address, MySQL root password and WordPress database name.
+
+## Installation
+
+Open a terminal and `cd` to the folder in which `docker-compose.yml` is saved and run:
+
+```
+docker-compose up
+```
+
+This creates two new folders next to your `docker-compose.yml` file.
+
+* `wp-data` – used to store and restore database dumps
+* `wp-app` – the location of your WordPress application
+
+The containers are now built and running. You should be able to access the WordPress installation with the configured IP in the browser address. By default it is `http://127.0.0.1:8000` (change in your hosts file).
+
+For convenience you may add a new entry into your hosts file.
+
+## Usage
+
+### Starting containers
+
+You can start the containers with the `up` command in daemon mode (by adding `-d` as an argument) or by using the `start` command:
+
+```
+docker-compose start
+```
+
+### Stopping containers
+
+```
+docker-compose stop
+```
+
+### Removing containers
+
+To stop and remove all the containers use the`down` command:
+
+```
+docker-compose down
+```
+
+Use `-v` if you need to remove the database volume which is used to persist the database:
+
+```
+docker-compose down -v
+```
+
+### Project from existing source
+
+Copy the `docker-compose.yml` file into a new directory. In the directory you create two folders:
+
+* `wp-data` – here you add the database dump
+* `wp-app` – here you copy your existing WordPress code
+
+You can now use the `up` command:
+
+```
+docker-compose up
+```
+
+This will create the containers and populate the database with the given dump. You may set your host entry and change it in the database, or you simply overwrite it in `wp-config.php` by adding:
+
+```
+define('WP_HOME','http://wp-app.local');
+define('WP_SITEURL','http://wp-app.local');
+```
+
+### Creating database dumps
+
+```
+./export.sh
+```
+
+### Developing a Theme
+
+Configure the volume to load the theme in the container in the `docker-compose.yml`:
+
+```
+volumes:
+  - ./theme-name/trunk/:/var/www/html/wp-content/themes/theme-name
+```
+
+### Developing a Plugin
+
+Configure the volume to load the plugin in the container in the `docker-compose.yml`:
+
+```
+volumes:
+  - ./plugin-name/trunk/:/var/www/html/wp-content/plugins/plugin-name
+```
+
+### WP CLI
+
+The docker compose configuration also provides a service for using the [WordPress CLI](https://developer.wordpress.org/cli/commands/).
+
+Sample command to install WordPress:
+
+```
+docker-compose run --rm wpcli core install --url=http://localhost --title=test --admin_user=admin --admin_email=test@example.com
+```
+
+Or to list installed plugins:
+
+```
+docker-compose run --rm wpcli plugin list
+```
+
+For an easier usage you may consider adding an alias for the CLI:
+
+```
+alias wp="docker-compose run --rm wpcli"
+```
+
+This way you can use the CLI command above as follows:
+
+```
+wp plugin list
+```
+
+### phpMyAdmin
+
+You can also visit `http://127.0.0.1:8080` to access phpMyAdmin after starting the containers.
+
+The default username is `root`, and the password is the same as supplied in the `.env` file.
---
+++ wp-app/wp-config.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * This has been slightly modified (to read environment variables) for use in Docker.
+ *
+ * @link https://wordpress.org/documentation/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// IMPORTANT: this file needs to stay in-sync with https://github.com/WordPress/WordPress/blob/master/wp-config-sample.php
+// (it gets parsed by the upstream wizard in https://github.com/WordPress/WordPress/blob/f27cb65e1ef25d11b535695a660e7282b98eb742/wp-admin/setup-config.php#L356-L392)
+
+// a helper function to lookup "env_FILE", "env", then fallback
+if (!function_exists('getenv_docker')) {
+	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
+	function getenv_docker($env, $default) {
+		if ($fileEnv = getenv($env . '_FILE')) {
+			return rtrim(file_get_contents($fileEnv), "\r\n");
+		}
+		else if (($val = getenv($env)) !== false) {
+			return $val;
+		}
+		else {
+			return $default;
+		}
+	}
+}
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'wordpress') );
+
+/** Database username */
+define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'example username') );
+
+/** Database password */
+define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'example password') );
+
+/**
+ * Docker image fallback values above are sourced from the official WordPress installation wizard:
+ * https://github.com/WordPress/WordPress/blob/1356f6537220ffdc32b9dad2a6cdbe2d010b7a88/wp-admin/setup-config.php#L224-L238
+ * (However, using "example username" and "example password" in your database is strongly discouraged.  Please use strong, random credentials!)
+ */
+
+/** Database hostname */
+define( 'DB_HOST', getenv_docker('WORDPRESS_DB_HOST', 'mysql') );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8') );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', getenv_docker('WORDPRESS_DB_COLLATE', '') );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         getenv_docker('WORDPRESS_AUTH_KEY',         '3387533ede4ab895ed56541dd33bec7cf256a49f') );
+define( 'SECURE_AUTH_KEY',  getenv_docker('WORDPRESS_SECURE_AUTH_KEY',  '1efd45436260f72f146670cded62945752d72617') );
+define( 'LOGGED_IN_KEY',    getenv_docker('WORDPRESS_LOGGED_IN_KEY',    'ad138a47ac8f27fd33879172f487c4b1e1481007') );
+define( 'NONCE_KEY',        getenv_docker('WORDPRESS_NONCE_KEY',        '6717df8b2848da8ca825a38e0bcb7ff4ef1f311a') );
+define( 'AUTH_SALT',        getenv_docker('WORDPRESS_AUTH_SALT',        '6d0c3fc6b82c664f2ce8069953e7ce5656ab2843') );
+define( 'SECURE_AUTH_SALT', getenv_docker('WORDPRESS_SECURE_AUTH_SALT', '2622e11d2e76a314b1071620a2bc95b9f871ca2e') );
+define( 'LOGGED_IN_SALT',   getenv_docker('WORDPRESS_LOGGED_IN_SALT',   'ccd9200667b2fdbe93accb758b24257da6269ffc') );
+define( 'NONCE_SALT',       getenv_docker('WORDPRESS_NONCE_SALT',       '3f5c746d8c7b3a5cc4408b4eda99f2250990f980') );
+// (See also https://wordpress.stackexchange.com/a/152905/199287)
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', '') );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also https://wordpress.org/support/article/administration-over-ssl/#using-a-reverse-proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
+	$_SERVER['HTTPS'] = 'on';
+}
+// (we include this by default because reverse proxying is extremely common in container environments)
+
+if ($configExtra = getenv_docker('WORDPRESS_CONFIG_EXTRA', '')) {
+	eval($configExtra);
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
